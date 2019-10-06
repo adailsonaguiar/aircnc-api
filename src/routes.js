56726ac26cb670ea991ab5a7cdc941ab02291622
Adailson Aguiar <adailsonacj@live.com>
2019-10-06 13:41:44 -0300

    feat(bookings): criar bookings

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ const uploadConfig = require('./config/upload');
 const SessionController = require('./controllers/Session');
 const SpotController = require('./controllers/Spot');
 const DashboardController = require('./controllers/Dashboard');
+const BookingController = require('./controllers/Booking');
 
 const upload = multer(uploadConfig);
 
@@ -17,5 +18,7 @@ routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 routes.get('/spots', SpotController.index);
 
 routes.get('/dashboard', DashboardController.show);
+
+routes.get('/spots/:spot_id/bookings', BookingController.store);
 
 module.exports = routes;
