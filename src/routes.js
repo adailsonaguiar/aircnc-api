9da68343af3cfcaec44034e745738352da374f4a
Adailson Aguiar <adailsonacj@live.com>
2019-10-05 23:46:35 -0300

    feat(upload): salvar fotos em pasta específica do app

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const routes = express.Router();
+
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+
 const SessionController = require('./controllers/Session');
+const SpotController = require('./controllers/Spot');
+
+const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
+
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 
 module.exports = routes;
